7a00084d4666cfa100571b725e4d2b67eb33396e
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-03-12 18:00:32 +0100

    feat: add ko-fi banner, github issue template, update footer, change to codewithshin.com

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,7 +30,7 @@
   description="Feather icon set for Svelte"
   openGraph={{
     type: 'website',
-    url: 'https://svelte-feathers.vercel.app/',
+    url: 'https://svelte-feathers.codewithshin.com/',
     title: 'Svelte Feathers',
     description: 'Feather icon set for Svelte',
     images: [
@@ -77,7 +77,7 @@
         <NavLi href="/" data-sveltekit-reload>Home</NavLi>
         <NavLi href="/icons">Icons</NavLi>
         <NavLi href="https://github.com/shinokada/svelte-feathers">GitHub</NavLi>
-        <NavLi href="https://svelte-svg-icons.vercel.app/">Icon sets</NavLi>
+        <NavLi href="https://svelte-svg-icons.codewithshin.com/">Icon sets</NavLi>
       </NavUl>
       <div class="flex items-center ml-auto">
         <DarkMode class="inline-block dark:hover:text-white hover:text-gray-900" />
@@ -92,8 +92,8 @@
   <Footer footerType="logo" class="dark:bg-stone-950">
     <div class="sm:flex sm:items-center sm:justify-between">
       <FooterBrand
-        href="https://svelte-feathers.vercel.app/"
-        name="Svelte Feathers"
+        href="https://codewithshin.com/"
+        name="codewithshin.com"
         classSpan="text-primary-700 dark:text-primary-500"
       />
       <FooterLinkGroup
